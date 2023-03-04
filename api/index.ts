524b21844c984ba527de7cadf0a965d2d1b1dd78
unknown <alexdang1993374@gmail.com>
2023-03-04 20:55:05 +0700

    feat: authentication

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
 require("dotenv").config();
@@ -8,6 +9,7 @@ require("dotenv").config();
 const app: Application = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "asdfawefasdf4eertertedrgesdfgsdfg35w4324wtsrftgh";
 
 app.use(express.json());
 app.use(
@@ -19,6 +21,7 @@ app.use(
 
 mongoose.connect(process.env.MONGO_URL as string);
 
+//endpoints
 app.get("/test", (req: Request, res: Response) => {
   res.json("test ok");
 });
@@ -35,6 +38,30 @@ app.post("/register", async (req: Request, res: Response) => {
     res.json(userDoc);
   } catch (e) {
     res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err: string, token: string) => {
+          if (err) throw err;
+
+          res.cookie("token", token).json("password Ok");
+        }
+      );
+    } else {
+      res.json("incorrect password");
+    }
+  } else {
+    res.json("email not found");
   }
 });
 
